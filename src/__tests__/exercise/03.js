e3a1501fe0e4fc62079ab6092f8cc85f844fdc41
Luci Temple <5368666+lucitemple@users.noreply.github.com>
2022-11-21 12:40:47 +1100

    Exercise 3: refactor to avoid implementation details

--- src/__tests__/exercise/03.js
+++ src/__tests__/exercise/03.js
@@ -10,10 +10,16 @@ import Counter from '../../components/counter'
 test('counter increments and decrements when the buttons are clicked', async () => {
   const user = userEvent.setup()
   render(<Counter />)
-  const [decrement, increment] = screen.getAllByRole('button')
+  const decrement = screen.getByRole('button', {
+    name: 'Decrement',
+  })
+  const increment = screen.getByRole('button', {
+    name: 'Increment',
+  })
+
   const message = screen.getByText('Current count: 0')
 
-  expect(message).toHaveTextContent('Current count: 0')
+  expect(message).toBeInTheDocument()
   await user.click(increment)
   expect(message).toHaveTextContent('Current count: 1')
   await user.click(decrement)
