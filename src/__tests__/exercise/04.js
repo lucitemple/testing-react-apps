8e53891e41d655965197333ac75da0391a4e9f34
Luci Temple <5368666+lucitemple@users.noreply.github.com>
2022-11-21 13:41:13 +1100

    Exercise 4 Extra credit: generate test data with faker

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -5,26 +5,33 @@ import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
+import {faker} from '@faker-js/faker'
+
+const buildLoginForm = () => {
+  return {
+    username: faker.internet.userName(),
+    password: faker.internet.password(),
+  }
+}
 
 test('submitting the form calls onSubmit with username and password', async () => {
   const user = userEvent.setup()
-  const userName = 'userName'
-  const userPassword = 'userPassword'
+  const {username, password} = buildLoginForm()
 
-  const handleSubmitMock = jest.fn().mockName('handleSubmit');
+  const handleSubmitMock = jest.fn().mockName('handleSubmit')
 
   render(<Login onSubmit={handleSubmitMock} />)
 
-  const username = screen.getByLabelText(/username/i)
-  const password = screen.getByLabelText(/password/i)
+  const usernameInput = screen.getByLabelText(/username/i)
+  const passwordInput = screen.getByLabelText(/password/i)
 
-  await user.type(username, userName)
-  await user.type(password, userPassword)
+  await user.type(usernameInput, username)
+  await user.type(passwordInput, password)
 
   await user.click(screen.getByRole('button'))
   expect(handleSubmitMock).toHaveBeenCalledWith({
-    password: userPassword,
-    username: userName,
+    password,
+    username,
   })
 })
 
